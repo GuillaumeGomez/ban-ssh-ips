ca7ff439edc0854d91219defa5a64091aa3d93ee
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-10-15 22:46:20 +0200

    Only switch to sudo when writing to hosts.deny file

--- ban_ips.py
+++ ban_ips.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+import os
+import subprocess
+
+
+def convert_to_string(s):
+    if s.__class__.__name__ == 'bytes':
+        return s.decode('utf-8')
+    return s
+
 
 def is_successful_login(line):
     return ": Accepted password " in line
@@ -10,7 +19,7 @@ def is_failed_login(line):
 
 
 # grep "Failed password" /var/log/auth.log
-def get_failed_ips():
+def get_failed_ips(current_ssh_ip):
     print('Getting failed IPs from /var/log/auth.log...')
     with open('/var/log/auth.log', 'r') as f:
         content = f.readlines()
@@ -28,6 +37,10 @@ def get_failed_ips():
         if len(ip) < 2:
             continue
         ip = ip[0]
+        if ip == current_ssh_ip:
+            if is_failure:
+                print('=> Found a failed login attempt for the current ssh session IP!')
+            continue
         if is_failure:
             if ip in success_logins:
                 continue
@@ -57,7 +70,7 @@ def get_failed_ips():
 
 def get_banned_ips():
     print('First, reading /etc/hosts.deny...')
-    with open('/etc/hosts.deny', 'r+') as f:
+    with open('/etc/hosts.deny', 'r') as f:
         content = f.readlines()
     saved_ips = {}
     for line in content:
@@ -71,24 +84,64 @@ def get_banned_ips():
 
 def save_new_banned_users(failed_ips, banned_ips):
     wrote_ips = 0
-    with open('/etc/hosts.deny', 'a') as f:
-        for x in failed_ips:
-            if x[0] in banned_ips:
+    command = "echo '"
+    for x in failed_ips:
+        if x[0] in banned_ips:
+            continue
+        if x[0].startswith('192.168.'):
+            i = input('{} seems to be a local address... Do you still want to ban it? [y/N] '
+                      .format(x[0]))
+            i = i.strip().lower()
+            if i != 'y':
+                print('Ignoring it then!')
                 continue
-            if x[0].startswith('192.168.'):
-                i = input('{} seems to be a local address... Do you still want to ban it? [y/N] '
-                          .format(x[0]))
-                i = i.strip().lower()
-                if i != 'y':
-                    print('Ignoring it then!')
-                    continue
-            f.write('sshd: {}\n'.format(x[0]))
-            wrote_ips += 1
+        command += 'sshd: {}\n'.format(x[0])
+        wrote_ips += 1
+    if wrote_ips > 0:
+        command = command[:-1] # remove last useless backline
+    command += "' >> /etc/hosts.deny"
+    if wrote_ips == 0:
+        print('Nothing left to write after deduplication, aborting...')
+        return
+    out = subprocess.run(['sudo', 'bash', '-c', command],
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    stdout = convert_to_string(out.stdout)
+    print(stdout)
+    if out.returncode != 0:
+        print('Command failed... If you want to run yourself:')
+        print('sudo bash -c {}'.format(command))
+        return
     print('Added {} new IPs to the /etc/hosts.deny file'.format(wrote_ips))
 
 
+def get_ssh_connection_ip():
+    print('Trying to get current ssh session IP...')
+    print("Let's start with SSH_CLIENT environment variable!")
+    ip = os.environ.get('SSH_CLIENT')
+    if ip is not None and len(ip) > 0:
+        ip = ip.split(' ')[0]
+        print("Found it! It's {}".format(ip))
+        return ip
+    print('Nothing there...')
+    print("Let's try with SSH_CONNECTION environment variable!")
+    ip = os.environ.get('SSH_CONNECTION')
+    if ip is not None and len(ip) > 0:
+        ip = ip.split(' ')[0]
+        print("Found it! It's {}".format(ip))
+    print("Nothing there either... Maybe it's not an ssh session?")
+    return None
+
+
 def main():
-    failed_ips = get_failed_ips()
+    if os.geteuid() == 0:
+        print("It'd be better to start this script not being root (it allows to keep the \
+                session variables)")
+        i = input('Do you still want to continue? [y/N] ').strip().lower()
+        if i != 'y':
+            print('Aborting')
+            return
+    failed_ips = get_failed_ips(get_ssh_connection_ip())
     if len(failed_ips) == 0:
         print('No failed attempts detected, leaving...')
         return
